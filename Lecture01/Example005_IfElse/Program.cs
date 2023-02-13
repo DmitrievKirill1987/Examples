afd00050b6aaebfbdbef374c665f1e5dcc8cf959
Kirill <frantic.87@mail.ru>
2023-02-13 16:02:05 +0300

    divided examples by lectures dir

---
+++ Lecture01/Example005_IfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "анита")
+{
+    Console.WriteLine("Ура, это же Анита!");
+
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
