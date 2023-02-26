72b5c130842070420068fda729215cdace631306
Kirill <frantic.87@mail.ru>
2023-02-26 17:27:37 +0300

    added Lecture 04

---
+++ Lecture04/Example016_FibonacciNumbers/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выводим числа Фибоначчи
+
+double Fibonacci(int n)
+{
+    if (n == 1) return 0;
+    else if (n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+for (int i = 1; i < 20; i++)
+{
+    Console.WriteLine($"{i} = {Fibonacci(i)}");
+}
